d00c759c502e931c11dd84ae2e5d2605cd65c295
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2021-06-01 13:27:36 +0800

    Add chapter 2 code

---
+++ tests/func/test_add_variety.py
@@ -0,0 +1,68 @@
+"""Test the tasks.add() API function."""
+
+import pytest
+import tasks
+from tasks import Task
+
+
+
+def equivalent(t1, t2):
+    """Check two tasks for equivalence."""
+    # Compare everything but the id field
+    return ((t1.summary == t2.summary) and
+            (t1.owner == t2.owner) and
+            (t1.done == t2.done))
+
+
+@pytest.fixture(autouse=True)
+def initialized_tasks_db(tmpdir):
+    """Connect to db before testing, disconnect after."""
+    tasks.start_tasks_db(str(tmpdir), 'tiny')
+    yield
+    tasks.stop_tasks_db()
+
+
+@pytest.mark.parametrize('summary, owner, done',
+                         [('sleep', None, False),
+                          ('wake', 'brian', False),
+                          ('breathe', 'BRIAN', True),
+                          ('eat eggs', 'BrIaN', False),
+                          ])
+def test_add_tuple(summary, owner, done):
+    """Demonstrate parametrize with multiple parameters."""
+    task = Task(summary, owner, done)
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
+
+tasks_to_try = (Task('sleep', done=True),
+                Task('wake', 'brian'),
+                Task('wake', 'brian'),
+                Task('breathe', 'BRIAN', True),
+                Task('exercise', 'BrIaN', False))
+
+
+
+task_ids = ['Task({},{},{})'.format(t.summary, t.owner, t.done)
+            for t in tasks_to_try]
+
+
+@pytest.mark.parametrize('task', tasks_to_try, ids=task_ids)
+def test_add_description(task):
+    """Demonstrate ids."""
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
+
+@pytest.mark.parametrize('task', [
+    pytest.param(Task('create'), id='just summary'),
+    pytest.param(Task('inspire', 'Michelle'), id='summary/owner'),
+    pytest.param(Task('encourage', 'Michelle', True), id='summary/owner/done')])
+def test_add_handcoded(task):
+    """Demonstrate pytest.param and id."""
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
